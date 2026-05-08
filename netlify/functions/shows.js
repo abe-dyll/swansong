83e711ea6329ae7d3187b1948a46d86156def535
abe-dyll <dylan.c.abraham@gmail.com>
2026-05-07 23:26:17 -0400

    Update shows.js

--- netlify/functions/shows.js
+++ netlify/functions/shows.js
@@ -1,21 +1,39 @@
 const TM_KEY = "NVPeiOI6iUQOfiXXxGjRdYPUIBedQmBy";
 
-exports.handler = async function(event) {
-  const params = event.queryStringParameters || {};
-  const artist = params.artist;
-  const state = params.state;
-  const lat = params.lat;
-  const lng = params.lng;
-  const radius = params.radius || "50";
+function isBadMatch(eventName, artistName) {
+  var lower = eventName.toLowerCase();
 
-  if (!artist) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "Missing artist param" })
-    };
+  // Always block anything with tribute in the name
+  if (lower.indexOf("tribute") !== -1) return true;
+
+  // Event must contain at least one significant word from the artist name
+  var artistWords = artistName.toLowerCase()
+    .replace(/^the /, "")
+    .split(" ")
+    .filter(function(w) { return w.length > 2; });
+
+  for (var i = 0; i < artistWords.length; i++) {
+    if (lower.indexOf(artistWords[i]) !== -1) return false;
   }
 
-  let locationParam = "";
+  // No artist name word found in event title - reject it
+  return true;
+}
+
+exports.handler = async function(event) {
+  var params = event.queryStringParameters || {};
+  var artist = params.artist;
+  var artistDisplay = params.artistDisplay || artist;
+  var state = params.state;
+  var lat = params.lat;
+  var lng = params.lng;
+  var radius = params.radius || "50";
+
+  if (!artist) {
+    return { statusCode: 400, body: JSON.stringify({ error: "Missing artist param" }) };
+  }
+
+  var locationParam = "";
   if (lat && lng) {
     locationParam = "&latlong=" + lat + "," + lng + "&radius=" + radius + "&unit=miles";
   } else if (state) {
@@ -24,56 +42,58 @@ exports.handler = async function(event) {
     locationParam = "&countryCode=US";
   }
 
-  const url = "https://app.ticketmaster.com/discovery/v2/events.json"
+  var url = "https://app.ticketmaster.com/discovery/v2/events.json"
     + "?keyword=" + encodeURIComponent(artist)
     + "&classificationName=music"
     + "&sort=date,asc"
-    + "&size=5"
+    + "&size=15"
     + locationParam
     + "&apikey=" + TM_KEY;
 
   try {
-    const res = await fetch(url);
-    const data = await res.json();
-    const events = (data && data._embedded && data._embedded.events) ? data._embedded.events : [];
+    var res = await fetch(url);
+    var data = await res.json();
+    var events = (data && data._embedded && data._embedded.events) ? data._embedded.events : [];
 
-    const shows = events.map(function(e) {
-      const venue = e._embedded && e._embedded.venues && e._embedded.venues[0];
-      const priceRanges = e.priceRanges && e.priceRanges[0];
-      let priceLevel = null;
+    var shows = [];
+    for (var i = 0; i < events.length; i++) {
+      var e = events[i];
+      var eventName = e.name || "";
+
+      if (isBadMatch(eventName, artistDisplay)) continue;
+
+      var venue = e._embedded && e._embedded.venues && e._embedded.venues[0];
+      var priceRanges = e.priceRanges && e.priceRanges[0];
+      var priceLevel = null;
       if (priceRanges && priceRanges.min) {
-        const min = priceRanges.min;
+        var min = priceRanges.min;
         if (min < 40) priceLevel = 1;
         else if (min < 80) priceLevel = 2;
         else if (min < 150) priceLevel = 3;
         else priceLevel = 4;
       }
-      return {
+
+      shows.push({
         date: e.dates && e.dates.start && e.dates.start.localDate,
         time: e.dates && e.dates.start && e.dates.start.localTime,
+        eventName: eventName,
         venue: venue && venue.name,
         city: venue && venue.city && venue.city.name,
         state: venue && venue.state && venue.state.stateCode,
         country: venue && venue.country && venue.country.name,
-        tour: e.classifications && e.classifications[0] && e.classifications[0].segment && e.classifications[0].segment.name,
-        eventName: e.name,
         priceLevel: priceLevel,
         url: e.url
-      };
-    });
+      });
+
+      if (shows.length >= 5) break;
+    }
 
     return {
       statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      },
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
       body: JSON.stringify(shows)
     };
   } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    };
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
   }
 };
